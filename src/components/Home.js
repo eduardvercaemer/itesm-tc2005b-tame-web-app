43d283e16733b32db83dac206799386bf6abe3b9
Vercaemer Eduard <vercaemereduard@gmail.com>
2022-05-01 23:28:26 -0500

    add home buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container className="mt-3">
+      <Row>
+        <Col className="mx-auto text-center">
+          <h1>Home</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={6} className="mx-auto text-center">
+          <Button  as={Link} to="/home/social" variant="primary">
+            Social
+          </Button>
+        </Col>
+        <Col xs={6} className="mx-auto text-center">
+          <Button as={Link} to="/home/profile" variant="primary">
+            Profile
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Home;
--- src/views/Layout.js
+++ src/views/Layout.js
@@ -1,71 +1,70 @@
-import React from 'react'
-import { useState } from 'react'
-import Nav from 'react-bootstrap/Nav'
-import { Container, Navbar } from 'react-bootstrap'
-import { NavLink, Link, Outlet, useNavigate } from 'react-router-dom'
-import { SidebarData } from '../components/SidebarData'
-import * as FaIcons from "react-icons/fa"
-import * as AiIcons from "react-icons/ai"
-import { IconContext } from 'react-icons';
-import './Layout.css'
-
+import React from "react";
+import { useState } from "react";
+import Nav from "react-bootstrap/Nav";
+import { Container, Navbar } from "react-bootstrap";
+import { NavLink, Link, Outlet, useNavigate } from "react-router-dom";
+import { SidebarData } from "../components/SidebarData";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { IconContext } from "react-icons";
+import "./Layout.css";
 
 const Layout = () => {
-   const navigate = useNavigate();
-   const handleLogout = () =>{
-       localStorage.removeItem('token'); //Eliminamos el token
-       navigate('/', {replace: true}); //El replace evita que se genere un histórico de haber iniciado sesión
- }
-   const [sidebar, setSidebar] = useState(false);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("token"); //Eliminamos el token
+    navigate("/", { replace: true }); //El replace evita que se genere un histórico de haber iniciado sesión
+  };
+  const [sidebar, setSidebar] = useState(false);
 
-   const showSidebar = () => setSidebar(!sidebar) //Cambia el estado en el que está al contrario (activa la sidebar)
+  const showSidebar = () => setSidebar(!sidebar); //Cambia el estado en el que está al contrario (activa la sidebar)
   return (
-
     <main>
-        
-        <>
-        <IconContext.Provider value={{ color: '#fff' }}>
-            <Navbar className='navbar'>
-                <Link to='#' className='menu-bars'>
-                    <FaIcons.FaBars onClick={showSidebar} />
+      <>
+        <IconContext.Provider value={{ color: "#fff" }}>
+          <Navbar className="navbar">
+            <Link to="#" className="menu-bars">
+              <FaIcons.FaBars onClick={showSidebar} />
+            </Link>
+            <Container
+              style={{ display: "flex", flexDirection: "row-reverse" }}
+            >
+              <Nav>
+                <Nav.Item>
+                  <Nav.Link>
+                    <div className="btn-logout" onClick={handleLogout}>
+                      <div className="logout-logo">
+                        <AiIcons.AiOutlineLogout />
+                      </div>
+                      Logout
+                    </div>
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Container>
+          </Navbar>
+          <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+            <ul className="nav-menu-items" onClick={showSidebar}>
+              <li className="navbar-toggle">
+                <Link to="#" className="menu-bars">
+                  <AiIcons.AiOutlineClose />
                 </Link>
-                <Container style={{display: 'flex', flexDirection: 'row-reverse'}}>
-                    <Nav>
-                        <Nav.Item>
-                            <Nav.Link>
-                                <div className='btn-logout' onClick={handleLogout}>
-                                    <div className='logout-logo'>
-                                        <AiIcons.AiOutlineLogout />
-                                    </div>
-                                    Logout
-                                </div>
-                            </Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                </Container>
-            </Navbar>
-            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-            <ul className='nav-menu-items' onClick={showSidebar}>
-                <li className='navbar-toggle'>
-                <Link to='#' className='menu-bars'>
-                    <AiIcons.AiOutlineClose />
-                </Link>
-                </li>
-                {SidebarData.map((item, index) => {
+              </li>
+              {SidebarData.map((item, index) => {
                 return (
-                    <li key={index} className={item.cName}>
+                  <li key={index} className={item.cName}>
                     <Link to={item.path}>
-                        {item.icon}
-                        <span>{item.title}</span>
+                      {item.icon}
+                      <span>{item.title}</span>
                     </Link>
-                    </li>
+                  </li>
                 );
-                })}
+              })}
             </ul>
-            </nav>
+          </nav>
         </IconContext.Provider>
-        </> 
-        {/* <Navbar bg='dark'>
+      </>
+      {/* <Navbar bg='dark'>
             <Container>
                 <Navbar.Brand>
                     <NavLink to='/home'>Escape from Reality</NavLink>
@@ -89,11 +88,11 @@ const Layout = () => {
                 </Nav>
             </Container>
         </Navbar> */}
-        <section>
-            <Outlet></Outlet>
-        </section>
+      <section>
+        <Outlet></Outlet>
+      </section>
     </main>
-  )
-}
+  );
+};
 
 export default Layout;
